a4ccca068c06c1d227adccbf6f2b64a945119118
VictorMontoya <vmon2712@gmail.com>
2023-10-17 08:32:49 -0500

    Entrega Sprint 2

---
+++ Home.js
@@ -0,0 +1,24 @@
+
+const informacion = document.getElementById("Contenedor")
+const arrayEevent = data.events
+
+
+for (let event of arrayEevent) {
+  const caja = document.createElement("div")
+  caja.classList.add("card")
+  caja.style.width = "18rem"
+
+  caja.innerHTML = `<img src="${event.image}" class="card-img-top tamaño" alt="">
+<div class="card-body">
+  <h5 class="card-title">"${event.name}"</h5>
+  <p class="card-text">"${event.description}"</p>
+  <p class="text-start d-flex position-absolute">"Price: ${event.price} "</p>
+  <div class="d-flex justify-content-end">
+    <a href="Details.html" class="btn btn-primary  ">Details</a>
+  </div>
+`
+
+  informacion.appendChild(caja)
+
+}
+
---
+++ PastEvents.js
@@ -0,0 +1,40 @@
+const informacion = document.getElementById("Contenedor")
+const arrayEevent = data.events
+
+
+
+const fechaActual = parseInt(data.currentDate)
+let upComing=[]
+
+for (let longitud = 0; longitud < 14; longitud++) {
+  const fechaEvents = parseInt(data.events[longitud].date);
+  if (fechaActual > fechaEvents) {upComing.push(data.events[longitud])
+  
+  }
+}console.log(upComing)
+
+
+
+for (let event of upComing) { 
+
+     
+     {
+
+        const caja = document.createElement("div")
+        caja.classList.add("card")
+        caja.style.width = "18rem"
+
+        caja.innerHTML = `<img src="${event.image}" class="card-img-top tamaño" alt="">
+<div class="card-body">
+  <h5 class="card-title">"${event.name}"</h5>
+  <p class="card-text">"${event.description}"</p>
+  <p class="text-start d-flex position-absolute">"Price: ${event.price} "</p>
+  <div class="d-flex justify-content-end">
+    <a href="Details.html" class="btn btn-primary  ">Details</a>
+  </div>
+`
+
+        informacion.appendChild(caja)
+
+    }
+} 
